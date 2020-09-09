8a895163db5763789c1bcc7a9a60dbc34a476d6c
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-09-09 20:19:36 +0200

    Only the selected page is visible now.

--- ioadapters/controllers/WrapperPageController.js
+++ ioadapters/controllers/WrapperPageController.js
@@ -2,6 +2,10 @@ let affixesLink;
 let highlightsLink;
 let resultLink;
 
+let highlightInstructionsPage;
+let affixPoolPage;
+let resultPage;
+
 function main() {
     setupUiActions();
 }
@@ -11,27 +15,48 @@ function setupUiActions() {
     highlightsLink = document.getElementById("highlightsLink");
     resultLink = document.getElementById("resultLink");
 
+    highlightInstructionsPage = document.getElementById('highlightInstructionsManagement');
+    affixPoolPage = document.getElementById('affixPoolInput');
+    resultPage = document.getElementById('result');
+
     affixesLink.onclick = onAffixesinkClicked;
     highlightsLink.onclick = onHighlightsLinkClicked;
     resultLink.onclick = onResultLinkClicked;
+
+    onAffixesinkClicked();
 }
 
 function onAffixesinkClicked() {
     affixesLink.className += ' active';
     highlightsLink.className = 'navlink';
     resultLink.className = 'navlink';
+
+    hideAllPages();
+    affixPoolPage.style.display = 'block';
 }
 
 function onHighlightsLinkClicked() {
     highlightsLink.className += ' active';
     affixesLink.className = 'navlink';
     resultLink.className = 'navlink';
+
+    hideAllPages();
+    highlightInstructionsPage.style.display = 'block';    
 }
 
 function onResultLinkClicked() {
     affixesLink.className = ' navlink';
     highlightsLink.className = 'navlink';
     resultLink.className += ' active';
+
+    hideAllPages();
+    resultPage.style.display = 'block';        
+}
+
+function hideAllPages() {
+    highlightInstructionsPage.style.display = 'none';
+    affixPoolPage.style.display = 'none';
+    resultPage.style.display = 'none';    
 }
 
 main();
